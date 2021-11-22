8c040cfde6514abacf29f17a3a7a17e5ae6261cb
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-11-22 22:01:29 +0100

    perfundimi i punes me parametra
    ka mbetur vetem enkriptimi dhe dekriptimi

--- ElGamalManagement.cs
+++ ElGamalManagement.cs
@@ -16,6 +16,65 @@ namespace ElGamal
         //celesi i tanishem qe po perdoret
         private ElGamalKeyStruct current_key;
 
+        public ElGamalManagement()
+        {
+            
+            current_key = new ElGamalKeyStruct();
+            
+            current_key.P = new BigInteger(0);
+            current_key.G = new BigInteger(0);
+            current_key.Y = new BigInteger(0);
+            current_key.X = new BigInteger(0);
+
+            // madhesia default e celesit
+            KeySizeValue = 384;
+
+            // rangu i celesave
+            LegalKeySizesValue = new[] { new KeySizes(384, 1088, 8) };
+        }
+
+        private void CreateKeyPair(int key_strength)
+        {
+            // krijimi i numrit random
+            Random random_number = new Random();
+
+            current_key.X = new BigInteger();
+            current_key.G = new BigInteger();
+
+            // krijimi i numrit prim P
+            current_key.P = BigInteger.genPseudoPrime(key_strength, 16, random_number);
+
+            // krijimi i dy numrave random, qe jane me te vegjel se P 
+            current_key.X.genRandomBits(key_strength - 1, random_number);
+            current_key.G.genRandomBits(key_strength - 1, random_number);
+
+            // llogaritja e Y modPow(exp, modulo) Y = GexpX modP
+            current_key.Y = current_key.G.modPow(current_key.X, current_key.P);
+        }
+
+        //Kontrollimi nese user-i nuk ka dhene input
+        private bool NeedToGenerateKey()
+        {
+            return current_key.P == 0 && current_key.G == 0 && current_key.Y == 0;
+        }
+
+        //Nese jo krijo ciftin e celesave
+        public ElGamalKeyStruct KeyStruct
+        {
+            get
+            {
+                if (NeedToGenerateKey())
+                {
+                    CreateKeyPair(KeySizeValue);
+                }
+                return current_key;
+            }
+            set
+            {
+                current_key = value;
+            }
+
+        }
 
         public override void ImportParameters(ElGamalParameters p_parameters)
         {
@@ -32,6 +91,41 @@ namespace ElGamal
             KeySizeValue = current_key.P.bitCount();
         }
 
+        public override ElGamalParameters ExportParameters(bool include_private_params)
+        {
+            //Nese nuk ka asgje per eksportim krijo ciftin e pare te celesave
+            if (NeedToGenerateKey())
+            {
+                CreateKeyPair(KeySizeValue);
+            }
 
+            ElGamalParameters elgamal_params = new ElGamalParameters();
+
+            elgamal_params.P = current_key.P.getBytes();
+            elgamal_params.G = current_key.G.getBytes();
+            elgamal_params.Y = current_key.Y.getBytes();
+
+            if (include_private_params)
+            {
+                elgamal_params.X = current_key.X.getBytes();
+            }
+            else
+            {
+                elgamal_params.X = new byte[1];
+            }
+
+            return elgamal_params;
+        }
+
+        // public override byte[] EncryptData(byte[] data)
+        // {
+            
+        // }
+
+        // public override byte[] DecryptData(byte[] p_data)
+        // {
+
+        // }
     }
+
 }
--- Parameters.cs
+++ Parameters.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
 using System.Security.Cryptography;
 
 namespace ElGamal
@@ -17,5 +20,65 @@ namespace ElGamal
         //Metoda: Import ElGamalParameters
         public abstract void ImportParameters(ElGamalParameters import_parameters);
        
+        //Metoda: Export ElGamalParameters
+        public abstract ElGamalParameters ExportParameters(bool export_parameters);
+
+        // public abstract byte[] EncryptData(byte[] _data);
+        // public abstract byte[] DecryptData(byte[] _data);
+
+        public override string ToXmlString(bool _private)
+        {
+            ElGamalParameters elgamal_params = ExportParameters(_private);
+         
+            StringBuilder string_builder = new StringBuilder();
+            string_builder.Append("<s--------------------KeyValues--------------------------------s>\n\n");
+
+            //elementet publike
+
+            string_builder.Append("<P>  " + Convert.ToBase64String(elgamal_params.P) + "  </P>\n\n");
+            string_builder.Append("<G>  " + Convert.ToBase64String(elgamal_params.G) + "  </G>\n\n");
+            string_builder.Append("<Y>  " + Convert.ToBase64String(elgamal_params.Y) + "  </Y>\n\n");
+
+            if (_private)
+            {
+                // perfshirja e celesit privat
+                string_builder.Append("<X>  " + Convert.ToBase64String(elgamal_params.X) + "  </X>\n\n");
+            }
+            string_builder.Append("</s--------------------KeyValues--------------------------------s>");
+            return string_builder.ToString();
+        }
+
+        public override void FromXmlString(String _string)
+        {
+            // krijimi i parametrave
+            ElGamalParameters elgamal_params = new ElGamalParameters();
+ 
+            XmlTextReader xml_text_reader = new XmlTextReader(new System.IO.StringReader(_string));
+
+            while (xml_text_reader.Read())
+            {
+                
+                if (true || xml_text_reader.IsStartElement())
+                {
+                    switch (xml_text_reader.Name)
+                    {
+                        case "P":
+                            elgamal_params.P = Convert.FromBase64String(xml_text_reader.ReadString());
+                            break;
+                        case "G":
+                            elgamal_params.G = Convert.FromBase64String(xml_text_reader.ReadString());
+                            break;
+                        case "Y":
+                            elgamal_params.Y = Convert.FromBase64String(xml_text_reader.ReadString());
+                            break;
+                        case "X":
+                            elgamal_params.X = Convert.FromBase64String(xml_text_reader.ReadString());
+                            break;
+                    }
+                }
+            }
+            
+            ImportParameters(elgamal_params);
+        }
     }
 }
