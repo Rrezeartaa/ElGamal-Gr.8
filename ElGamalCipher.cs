d363d7e84d10a27b2d31f39e6b4432026a8020e5
Rrezearta <rrezearta.thaqi@student.uni-pr.edu>
2021-11-24 23:06:51 +0100

    procesimi i te dhenave te bllokut

--- ElGamalCipher.cs
+++ ElGamalCipher.cs
@@ -12,8 +12,9 @@ namespace ElGamal
         protected int block_size;
         protected int plaintext_blocksize;
         protected int ciphertext_blocksize;
-
         protected ElGamalKeyStruct current_key;
+        protected abstract byte[] ProcessDataBlock(byte[] plaintext_data_block);
+        protected abstract byte[] ProcessFinalDataBlock(byte[] final_plaintext_block);
 
         public ElGamalCipher(ElGamalKeyStruct elg_current_key)
         {
@@ -22,12 +23,46 @@ namespace ElGamal
 
             // llogaritja e madhesise se blloqeve
             plaintext_blocksize = (elg_current_key.P.bitCount() - 1) / 8;
-            ciphertext_blocksize = ((elg_current_key.P.bitCount() + 7) / 8) * 2; //ciphertext is twice as plaintext
+            ciphertext_blocksize = ((elg_current_key.P.bitCount() + 7) / 8) * 2; 
 
             // vendosja e bllokut default per tekstin origjinal
             block_size = plaintext_blocksize;
         }
 
-        
+         public byte[] ProcessData(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream();
+
+            // llogaritja e blloqeve te kompletuara
+            int complete_blocks = data.Length / block_size;
+
+            byte[] hold_data_block = new Byte[block_size];
+
+            int i = 0;
+
+            for (; i < complete_blocks; i++)
+            {
+                // kopjimi i bllokut dhe krijimi i big integer
+                Array.Copy(data, i * block_size, hold_data_block, 0, block_size);
+                // procesimi i bllokut
+                byte[] result = ProcessDataBlock(hold_data_block);
+                // shkruarja e te dhenave te procesuara ne stream
+                stream.Write(result, 0, result.Length);
+            }
+
+            // procesimi i bllokut final
+            byte[] last_block = new Byte[data.Length - (complete_blocks * block_size)];
+
+            Array.Copy(data, i * block_size, last_block, 0, last_block.Length);
+
+            // procesimi i bllokut final
+            byte[] final_result = ProcessFinalDataBlock(last_block);
+
+            // shkruarja e bajtave te fundit te te dhenave ne stream
+            stream.Write(final_result, 0, final_result.Length);
+
+            // kthimi i permbajtjeve te stream si nje byte array 
+            return stream.ToArray();
+        }
     }
 }
